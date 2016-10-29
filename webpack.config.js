467ad69a48a3c0cbb0a52ed914f7fefa92b59d8f
PhanVanKien <phanphanphan1993@gmail.com>
2016-10-30 00:06:45 +0700

    Stupid learn

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: __dirname + '/app/index.html',
+  filename: 'index.html',
+  inject: 'body'
+});
+
+module.exports = {
+  entry: [
+    './app/index.js'
+  ],
+  output: {
+    path: __dirname + '/dist',
+    filename: "index_bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/, exclude: /node_modules/, loader: "babel-loader",
+        query:
+        {
+            presets:['react']
+        }
+      }
+    ]
+  },
+  plugins: [HTMLWebpackPluginConfig],
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './',
+    inline: true,
+    port : 3000
+  }
+};
